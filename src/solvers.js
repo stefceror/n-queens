5442d477316c6354aec7f2c432675f070f3da2c2
stefceror <stefsnow@gmail.com>
2014-07-30 21:01:34 -0700

    Started solution for countNRooks

--- src/solvers.js
+++ src/solvers.js
@@ -13,14 +13,14 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-window.findNRooksSolution = function(n) {
+window.findNRooksSolution = function(n, startingPos) {
   var solution = [];
   var row = [];
-  var rookPosition = 0;
+  rookPosition = startingPos || 0;
   for( var j = 0; j < n; j++ ){
     row = [];
     for( var i = 0; i < n; i++ ){
-      if( i === rookPosition ){
+      if( i === rookPosition % n ){
         row.push(1);
       }else{
         row.push(0);
@@ -36,14 +36,71 @@ window.findNRooksSolution = function(n) {
 
 
 
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+
+
+
+
+
+
+// return the number of nxn chessboards 'that' exist, with n rooks
+
+// placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var possibleBoards = [];
+  var baseRows = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]];
+  var finalBoards = [];
+  var newBoard = [];
+
+
+//  9pm Wed game plan:
+//
+// loop through possibleBoards
+// append baseRows[1] to each element
+// copy magic
+// append baseRows[2] to each element (that came from copy magic)
+// concatenate the two different arrays
+// set that concatenation equal to possibleBoards
+// call/do above a total of n times
+
+debugger;
+        for(var i = 0; i < baseRows.length; i++){
+          newBoard = [];
+          newBoard.push(baseRows[i]);
+          possibleBoards.push(newBoard);
+        };
+        for( var k = 1; k < n; k++ ){
+
+          // for each element in possibleBoards
+          // append baseRows[i]
+          for( var i = 0; i < possibleBoards.length; i++ ){
+            for( var j = 0; j < baseRows.length; j++ ){
+              possibleBoards[i].push(baseRows[j]);
+            };
+          };
+
+        };
+
+
+
+//   for( var i = 0; i < n; i++){
+//     for( var j = 0; j < n; j++){
+//       for( var k = 0; k < n; k++){
+//         for( var l = 0; l < n; l++){
+//           possibleBoards.push(new Board([baseRows[i], baseRows[j], baseRows[k], baseRows[l]]));
+//         }
+//       }
+//     }
+//   }
+  for( var i = 0; i < finalBoards.length; i++ ){
+    if( !finalBoards[i].hasAnyRowConflicts() && !finalBoards[i].hasAnyColConflicts() ){
+      solutionCount++;
+    }
+  }
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
-
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
